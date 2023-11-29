478d0f973bd1d174a8d05ef40294ca4ce31279bb
Vladimir Shtefan <vladimir.s@upland.me>
2023-11-29 17:59:58 +0100

    feat(config-loader): added env variables support

--- public/stream/config_loader.go
+++ public/stream/config_loader.go
@@ -2,12 +2,25 @@ package stream
 
 import (
 	"blink/config"
+	"bytes"
+	"os"
+	"regexp"
+	"strings"
+
 	"gopkg.in/yaml.v3"
 )
 
+var (
+	envRegex        = regexp.MustCompile(`\${[0-9A-Za-z_.]+(:((\${[^}]+})|[^}])*)?}`)
+	escapedEnvRegex = regexp.MustCompile(`\${({[0-9A-Za-z_.]+(:((\${[^}]+})|[^}])*)?})}`)
+)
+
 func ReadInitConfigFromYaml(configBytes []byte) (config.Configuration, error) {
+	replacedBytes := replaceEnvVariables(configBytes, os.LookupEnv)
+
 	conf := config.Configuration{}
-	err := yaml.Unmarshal(configBytes, &conf)
+	err := yaml.Unmarshal(replacedBytes, &conf)
+
 	config.ValidateConfigSchema(conf)
 	return conf, err
 }
@@ -17,7 +30,35 @@ func ReadDriverConfig[T any](driverConfig interface{}, targetConfig T) (T, error
 	if err != nil {
 		return targetConfig, err
 	}
-	err = yaml.Unmarshal(marshaled, &targetConfig)
+
+	replacedBytes := replaceEnvVariables(marshaled, os.LookupEnv)
+
+	err = yaml.Unmarshal(replacedBytes, &targetConfig)
 
 	return targetConfig, err
+}
+
+func replaceEnvVariables(config []byte, lookupFunc func(string) (string, bool)) []byte {
+	replaced := envRegex.ReplaceAllFunc(config, func(content []byte) []byte {
+		var value string
+
+		if len(content) > 3 {
+			if colonIndex := bytes.IndexByte(content, ':'); colonIndex == -1 {
+				varName := string(content[2 : len(content)-1])
+				value, _ = lookupFunc(varName)
+			} else {
+				targetVar := content[2:colonIndex]
+				defaultVal := content[colonIndex+1 : len(content)-1]
+				value, _ := lookupFunc(string(targetVar))
+				if value == "" {
+					value = string(defaultVal)
+				}
+			}
+			value = strings.ReplaceAll(value, []byte("$$$1"))
+		}
+
+		return []byte(value)
+	})
+
+	return replaced
 }
